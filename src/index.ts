5b0c55238a2c8a685f82af980ed70dfff93f8f3b
Mark Wylde <129744061+markwylde-deriv@users.noreply.github.com>
2023-05-05 18:02:04 +0100

    Fix bucketing logic

--- src/index.ts
+++ src/index.ts
@@ -39,9 +39,11 @@ export default function init (config: TestConfig[]): void {
 
     if (!chosenVariant) {
       const rand = Math.random();
+      let cumulativeProbability = 0;
       chosenVariant = 'control';
       for (const variant in test.variants) {
-        if (rand <= test.variants[variant]) {
+        cumulativeProbability += test.variants[variant];
+        if (rand <= cumulativeProbability) {
           chosenVariant = variant;
           break;
         }
