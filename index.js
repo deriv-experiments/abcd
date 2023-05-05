8c1167b51a4164fe7abf33d2c74a850371ff8f27
markwylde-deriv <129744061+markwylde-deriv@users.noreply.github.com>
2023-05-05 15:47:12 +0000

    Deploying to gh-pages from @ deriv-experiments/abcd@ca37f04aac3d5463879e9c6338665c4f9dd3c9b3 🚀

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+// src/getCookieValue.ts
+function getCookieValue(name) {
+  if (!name || typeof name !== "string") {
+    throw new Error("Invalid cookie name provided");
+  }
+  const namePattern = /^[^;=\s]+$/;
+  if (!namePattern.test(name)) {
+    throw new Error("Invalid characters in cookie name");
+  }
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const decodedValue = decodeURIComponent(parts.pop()?.split(";").shift() || "");
+    if (decodedValue) {
+      return decodedValue;
+    } else {
+      throw new Error("Error decoding cookie value");
+    }
+  }
+}
+
 // src/index.ts
 function ensureStyleAppended() {
+  if (document.getElementById("abTestStyles")) {
+    return;
+  }
   const style = globalThis.document.createElement("style");
-  style.textContent = `[ab-test-variant]:not([ab-test-variant="control"]) { display: none; }`;
+  style.id = "abTestStyles";
   globalThis.document.head.appendChild(style);
+  insertCssRule('[ab-test-variant]:not([ab-test-variant="control"]) { display: none; }');
+}
+function insertCssRule(rule) {
+  const stylesheet = document.getElementById("abTestStyles");
+  const sheet = stylesheet.sheet;
+  sheet.insertRule(rule, sheet.cssRules.length);
 }
 function init(config) {
   ensureStyleAppended();
   for (const test of config) {
     setupTest(test);
   }
-  applyAbTests();
   function setupTest(test) {
     const cookieName = `ab-${test.name}`;
     let chosenVariant = getCookieValue(cookieName);
@@ -25,25 +54,9 @@ function init(config) {
       }
       globalThis.document.cookie = `${cookieName}=${chosenVariant}; path=/; max-age=2592000`;
     }
-  }
-  function applyAbTests() {
-    const testElements = document.querySelectorAll("[ab-test-name]");
-    for (const element of testElements) {
-      const testName = element.getAttribute("ab-test-name");
-      const testVariant = element.getAttribute("ab-test-variant");
-      const chosenVariant = getCookieValue(`ab-${testName}`);
-      if (testVariant === chosenVariant) {
-        element.style.display = "inherit";
-      } else {
-        element.style.display = "none";
-      }
-    }
-  }
-  function getCookieValue(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-      return parts.pop()?.split(";").shift();
+    for (const variant in test.variants) {
+      const rule = `[ab-test-name="${test.name}"][ab-test-variant="${variant}"] { display: ${variant === chosenVariant ? "inherit" : "none"}; }`;
+      insertCssRule(rule);
     }
   }
 }
